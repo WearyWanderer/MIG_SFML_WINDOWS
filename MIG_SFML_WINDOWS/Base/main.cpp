2b0d737986a3aad961e3c74d4409a93735f54a77
Andrew Ayre <n3078472@live.tees.ac.uk>
2016-10-20 20:44:13 +0100

    basic semi-variable timestep

--- MIG_SFML_WINDOWS/Base/main.cpp
+++ MIG_SFML_WINDOWS/Base/main.cpp
@@ -1,12 +1,20 @@
 #include "stdafx.h"
 
+#include <iostream>
+
 #include "../Graphics/TextureLibrary.h"
 #include "../Entity.h"
 
 int main()
 {
+
+#pragma region Startup (window, clock etc.)
 	sf::RenderWindow window(sf::VideoMode(700, 700), "SFML works!");
-	
+	sf::Clock clock;
+	float currentTime = clock.getElapsedTime().asSeconds();
+	float dt = 1 / 60.0f, t = 0.0f;
+#pragma endregion
+
 	TextureLibrary bgTextures;
 	bgTextures.AddTexture("Art/cloud1.png");
 	bgTextures.AddTexture("Art/cloud4.png");
@@ -25,10 +33,25 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		testCloud.m_sprite.move(1, 1);
+
+		float newTime = clock.getElapsedTime().asSeconds();
+		float frameTime = newTime - currentTime;
+		currentTime = newTime;
+
+		while (frameTime > 0.0f)
+		{
+			float deltaTime = std::min(frameTime, dt);
+			//do anything that should be controlled by time instead of per frame here
+			frameTime -= deltaTime;
+			t += deltaTime;
+		}
+
+
+#pragma region Rendering
 		window.clear(sf::Color(142, 185, 255, 255));
 		window.draw(testCloud.m_sprite);
 		window.display();
+#pragma endregion
 	}
 
 	return 0;
